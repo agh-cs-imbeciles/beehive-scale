c9cd457af7616d3571d58107febee981f495c230
kpiotr6 <piotrek@komlan.pl>
2024-05-08 16:50:02 +0200

    Add config files

---
+++ scripts/activate.sh
@@ -0,0 +1,1 @@
+. $IDF_FOLDER/export.sh
---
+++ scripts/flash_filesystem.sh
@@ -0,0 +1,4 @@
+mkdir -p tmp
+scripts/mkspiffs -c esp32_config_files -b 4096 -p 256 -s 0x160000 tmp/spiffs.bin
+esptool.py --chip esp32 --port ${PORT} --baud 115200  write_flash -z 0x290000 tmp/spiffs.bin
+rm tmp/spiffs.bin
